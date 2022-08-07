40e11f5b9ef5540cb68c51f39d8736111f191f9d
Daniela Tonello <daniela.a.tonello@gmail.com>
2022-08-07 14:03:40 -0300

    pokedex

---
+++ index.js
@@ -0,0 +1,12 @@
+async function buscarPokemon() {
+    const inputValue = document.getElementById("idPokemon").value;
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`); //async away
+    const json = await data.json();
+
+    console.log(data);
+
+    if (data.status == 200) {
+      document.getElementById("name").innerText = json.name;
+      const inputValue = document.getElementById("idPokemon").value = "";
+    };   
+}
